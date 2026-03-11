116669d7e2bac63a87da0d4c2e7b65c17ad6296b
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2026-03-11 21:26:46 +0100

    feat(api): return json

--- api/dashboard.go
+++ api/dashboard.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -30,12 +31,13 @@ func DashboardHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "CPU Usage: %.2f%%\n", cpuInfo.Usage)
-	fmt.Fprintf(w, "CPU Temperature: %.2f°C\n", cpuInfo.Temperature)
-	fmt.Fprintf(w, "RAM Usage: %.2f%%\n", ramInfo.Usage)
-	fmt.Fprintf(w, "RAM Total: %.2f GB\n", float64(ramInfo.Total)/1024/1024/1024)
-	fmt.Fprintf(w, "Disk Usage: %.2f%%\n", diskInfo.Usage)
-	fmt.Fprintf(w, "Disk Total: %.2f GB\n", float64(diskInfo.Total)/1024/1024/1024)
-	fmt.Fprintf(w, "Network Bytes Sent: %.2f MB\n", float64(networkInfo.BytesSent)/1024/1024)
-	fmt.Fprintf(w, "Network Bytes Received: %.2f MB\n", float64(networkInfo.BytesReceived)/1024/1024)
+	data := map[string]any{
+		"cpu":     cpuInfo,
+		"ram":     ramInfo,
+		"disk":    diskInfo,
+		"network": networkInfo,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
 }
--- api/types.go
+++ api/types.go
@@ -1,21 +1,21 @@
 package api
 
 type CPUInfo struct {
-	Usage       float64
-	Temperature float64
+	Usage       float64 `json:"usage"`
+	Temperature float64 `json:"temperature"`
 }
 
 type RAMInfo struct {
-	Usage float64
-	Total uint64
+	Usage float64 `json:"usage"`
+	Total uint64  `json:"total"`
 }
 
 type DiskInfo struct {
-	Usage float64
-	Total uint64
+	Usage float64 `json:"usage"`
+	Total uint64  `json:"total"`
 }
 
 type NetworkInfo struct {
-	BytesSent     uint64
-	BytesReceived uint64
+	BytesSent     uint64 `json:"bytesSent"`
+	BytesReceived uint64 `json:"bytesReceived"`
 }
