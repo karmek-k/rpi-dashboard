865b0fdd85c7711e9813ae11c09db9f240ea0eb5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2026-03-11 20:41:31 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Raspberry Pi Dashboard")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	fmt.Println("Server is running on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
